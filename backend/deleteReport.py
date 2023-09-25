116dab395346d6fe4fdd7a7bd2e630b3fdb2a56d
unknown <abdul.m0701@gmail.com>
2023-09-25 13:05:17 -0400

    backend CI/CD test

--- backend/deleteReport.py
+++ backend/deleteReport.py
@@ -6,7 +6,7 @@ def deleteReport(bucketName, objName, s3):
     # exception handling with boto3 api call
     try:
         s3.delete_object(Bucket=bucketName, Key=objName)
-        return json.dumps(str(objName) + " Successfully Deleted!!!")
+        return json.dumps(str(objName) + " Successfully Deleted!!!!")
     
     except Exception as e:
         return json.dumps('ERROR: ' + str(e))
