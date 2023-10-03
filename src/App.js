39e63c0704924b74fffcb4b66fca43d5154c3521
unknown <abdul.m0701@gmail.com>
2023-10-03 01:07:58 -0400

    cluster potentially fixed, deployment test

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@
 // Version 1.1.0
 // CI/CD pipelines complete for both EKS and Lambda
 
+
 import './App.css';
 import EconTable from './components/EconTable'
 import NonEconTable from './components/NonEconTable';
