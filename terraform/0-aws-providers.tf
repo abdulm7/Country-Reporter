73e3d16ab5c36a9825bfa1671970882426595ed1
unknown <abdul.m0701@gmail.com>
2023-10-07 17:22:44 -0400

    terraform infrastrcture complete

---
+++ terraform/0-aws-providers.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+    region = "ca-central-1"
+}
+
+terraform {
+  required_version = "~> 1.0"
+
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+
+    helm = {
+        source = "hashicorp/helm"
+        version = ">= 2.6.0"
+    }
+  }
+}
---
+++ terraform/2-eks.tf
@@ -0,0 +1,45 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "18.29.0"
+
+  cluster_name    = "cr-cluster"
+  cluster_version = "1.25"
+
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  eks_managed_node_groups = {
+    general = {
+      desired_size = 1
+      min_size     = 1
+      max_size     = 6
+
+      labels = {
+        role = "on-demand"
+      }
+
+      instance_types = ["t3.micro"]
+      capacity_type  = "ON_DEMAND"
+
+    }
+
+    spot = {
+      desired_size = 1
+      min_size     = 1
+      max_size     = 6
+
+      labels = {
+        role = "spot"
+      }
+
+      instance_types = ["t3.micro"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+}
